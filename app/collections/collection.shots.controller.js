7cb7bca69f9f15503fccd5e56466623a06cba958
Vladimir Glushchenko <v.glushchenko@gmail.com>
2017-08-04 00:24:30 +0300

    preload_gif

--- app/collections/collection.shots.controller.js
+++ app/collections/collection.shots.controller.js
@@ -15,6 +15,17 @@
             shots: CollectionModel.getCollectionShots.get({id: $stateParams.id})
         };
 
+        activate();
+
+        function activate() {
+            let arr = vm.model.shots.shots;
+            for(let i=0; i < vm.model.shots.shots.length; i++) {
+                if (arr[i]['avatar_content_type'] == 'image/gif') {
+                    arr[i]['preloadGif'] = (new Image().src = arr[i]['avatar']);
+                }
+            }
+        }
+
         vm.menu = {
             showShot: showShot
         };
