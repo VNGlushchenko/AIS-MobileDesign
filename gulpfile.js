d3a15f253e5a06c6730c20fa227136ad35f1863d
Vladimir Glushchenko <v.glushchenko@gmail.com>
2017-08-21 10:19:20 +0300

    added style.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
-let gulp = require('gulp');
-gulp.task('hello-gulp', function() {
-    console.log('This is my first gulp task');
+const gulp   = require('gulp');
+const jshint = require('gulp-jshint');
+const jscs   = require('gulp-jscs');
+const concat = require('gulp-concat');
+
+gulp.task('vet', function() {
+    return gulp.src('app/**/*.js')
+        .pipe(jshint({esversion: 6}))
+        .pipe(jshint.reporter('default'))
+        .pipe(jscs())
+        .pipe(jscs.reporter());
 });
