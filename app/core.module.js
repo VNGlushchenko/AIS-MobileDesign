23f0c4e9f3b5f3095b98fa82824328750f369c0a
Vladimir Glushchenko <v.glushchenko@gmail.com>
2017-10-12 18:38:33 +0300

    sign-in form was impoved

--- app/core.module.js
+++ app/core.module.js
@@ -1,45 +1,65 @@
-;
 (function() {
-    "use strict";
+  'use strict';
 
-    angular.module("app", ['ui.router', 'ngResource', 'ngDialog', 'ngMessages', 'toastr'])
-        .run(['toastrConfig', '$rootScope', function(toastrConfig, $rootScope) {
-            angular.extend(toastrConfig, {
-                closeButton: true,
-                onHidden: function(isHiddenByClick, toast) {
-                    if (toast.scope.message == "For authorized users only") {
-                        $rootScope.$emit('authToastrClosed');
-                    }
-                }
-            });
-        }])
-        .config(['$resourceProvider', '$httpProvider', function($resourceProvider, $httpProvider) {
-            $resourceProvider.defaults.actions.saveWithHeaders = {
-                method: 'POST',
-                transformResponse: function(data, headers) {
-                    return {
-                        data: JSON.parse(data)['data'],
-                        errors: JSON.parse(data)['errors'],
-                        headers: headers()
-                    }
-                }
-            };
-
-            $httpProvider.interceptors.push('userCredential');
-        }])
-        .factory('userCredential', ['$injector', function($injector) {
-            return {
-                request: function(config) {
-                    let userAuthData = $injector.get('UserModel').model;
-                    if (!userAuthData.isUserAuthKeysEmpty) {
-                        config.headers = userAuthData.user;
-                    }
-                    console.log('url: ' + config.url + ' | method: ' + config.method + ' | headers: ', config.headers);
-                    return config;
-                }
+  angular
+    .module('app', [
+      'ui.router',
+      'ngResource',
+      'ngDialog',
+      'ngMessages',
+      'toastr'
+    ])
+    .run([
+      'toastrConfig',
+      '$rootScope',
+      function(toastrConfig, $rootScope) {
+        angular.extend(toastrConfig, {
+          closeButton: true,
+          onHidden: function(isHiddenByClick, toast) {
+            if (toast.scope.message == 'For authorized users only') {
+              $rootScope.$emit('authToastrClosed');
             }
-        }])
-        .config(['$qProvider', function ($qProvider) {
-            $qProvider.errorOnUnhandledRejections(false);
-        }]);
+          }
+        });
+      }
+    ])
+    .config([
+      '$resourceProvider',
+      '$httpProvider',
+      function($resourceProvider, $httpProvider) {
+        $resourceProvider.defaults.actions.saveWithHeaders = {
+          method: 'POST',
+          transformResponse: function(data, headers) {
+            return {
+              data: JSON.parse(data)['data'],
+              errors: JSON.parse(data)['errors'],
+              headers: headers()
+            };
+          }
+        };
+
+        $httpProvider.interceptors.push('userCredential');
+      }
+    ])
+    .factory('userCredential', [
+      '$injector',
+      function($injector) {
+        return {
+          request: function(config) {
+            let userAuthData = $injector.get('UserModel').model;
+            if (!userAuthData.isUserAuthKeysEmpty) {
+              config.headers = userAuthData.user;
+            }
+            //console.log('url: ' + config.url + ' | method: ' + config.method + ' | headers: ', config.headers);
+            return config;
+          }
+        };
+      }
+    ])
+    .config([
+      '$qProvider',
+      function($qProvider) {
+        $qProvider.errorOnUnhandledRejections(false);
+      }
+    ]);
 })();
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,97 +1,110 @@
-;
 (function() {
+  'use strict';
 
-    "use strict";
+  angular.module('app').factory('UserModel', UserModel);
 
-    angular
-        .module("app")
-        .factory("UserModel", UserModel);
+  UserModel.$inject = [
+    '$resource',
+    '$state',
+    'toastr',
+    '$q',
+    '$timeout',
+    '$rootScope'
+  ];
 
-    UserModel.$inject = ['$resource', '$state', 'toastr', '$q', '$timeout', '$rootScope'];
+  function UserModel($resource, $state, toastr, $q, $timeout, $rootScope) {
+    let vm = this;
 
-    function UserModel($resource, $state, toastr, $q, $timeout, $rootScope) {
-        let vm = this;
+    vm.model = {
+      user: {
+        accesstoken: '',
+        client: '',
+        expiry: '',
+        uid: '',
+        tokentype: ''
+      },
+      isUserAuthKeysEmpty: true,
+      serverValidationMessage: ''
+    };
 
-        vm.model = {
-            user: {
-                accesstoken: '',
-                client: '',
-                expiry: '',
-                uid: '',
-                tokentype: ''
-            },
-            isUserAuthKeysEmpty: true
-        };
+    vm.menu = {
+      signUp: signUp,
+      signIn: signIn,
+      logOut: logOut,
+      checkUserAuthData: checkUserAuthData,
+      setUserAuthData: setUserAuthData
+    };
 
-        vm.menu = {
-            signUp: signUp,
-            signIn: signIn,
-            logOut: logOut,
-            checkUserAuthData: checkUserAuthData,
-            setUserAuthData: setUserAuthData
-        };
+    vm.errorsMessages = {
+      signUp: {},
+      signIn: {},
+      feedback: {},
+      createShot: {}
+    };
 
-        vm.errorsMessages = {
-            signUp: {},
-            signIn: {},
-            feedback: {},
-            createShot: {}
-        };
+    return {
+      menu: vm.menu,
+      model: vm.model,
+      errorsMessages: vm.errorsMessages
+    };
 
-        return {
-            menu: vm.menu,
-            model: vm.model,
-            errorsMessages: vm.errorsMessages
-        }
+    function signUp(enteringData) {
+      return $resource('http://dev-api.mobile.design/api/auth')
+        .saveWithHeaders(enteringData)
+        .$promise.then(
+          response => redirectAfterAuth(response),
+          response => {
+            let errors = response.data.errors;
+            //email
+            if (errors.email.length > 1) {
+              vm.errorsMessages.signUp.email = errors.email[1];
+            } else {
+              vm.errorsMessages.signUp.email = errors.email[0];
+            }
 
-        function signUp(enteringData) {
-            return $resource('http://dev-api.mobile.design/api/auth').saveWithHeaders(enteringData).$promise.then(
-                response => redirectAfterAuth(response),
-                response => {
-                    let errors = response.data.errors;
-                    //email
-                    if (errors.email.length > 1) {
-                        vm.errorsMessages.signUp.email = errors.email[1];
-                    } else {
-                        vm.errorsMessages.signUp.email = errors.email[0];
-                    }
+            //password
+            if (errors.password.length) {
+              vm.errorsMessages.signUp.password = errors.password[0];
+            }
+            //password_confirmation
+            if (errors.password_confirmation.length) {
+              vm.errorsMessages.signUp.password_confirmation =
+                errors.password_confirmation[0];
+            }
+            console.log(response);
+            console.dir(vm.errorsMessages.signUp);
+            return $q.reject();
+          }
+        );
+    }
 
-                    //password
-                    if (errors.password.length) {
-                        vm.errorsMessages.signUp.password = errors.password[0];
-                    } 
-                    //password_confirmation
-                    if (errors.password_confirmation.length) {
-                       vm.errorsMessages.signUp.password_confirmation = errors.password_confirmation[0];
-                    }
-                    console.log(response);
-                    console.dir(vm.errorsMessages.signUp);
-                    return $q.reject();
-                }
-            );
-        }
+    function signIn(enteringData) {
+      return $resource('http://dev-api.mobile.design/api/auth/sign_in')
+        .saveWithHeaders(enteringData)
+        .$promise.then(
+          response => {
+            vm.model.serverValidationMessage = '';
+            redirectAfterAuth(response);
+          },
+          response => {
+            vm.model.serverValidationMessage = response.data.errors[0];
+          }
+        );
+    }
 
-        function signIn(enteringData) {
-            return $resource('http://dev-api.mobile.design/api/auth/sign_in').saveWithHeaders(enteringData).$promise.then(
-                response => redirectAfterAuth(response),
-                response => {
-                    // signInForm.$invalid = true;
-                    console.log(response);
-                }
-            );
-        }
+    function logOut() {
+      vm.model.user = {};
+      vm.model.isUserAuthKeysEmpty = true;
+      $state.go('signIn');
+    }
 
-        function logOut() {
-            vm.model.user = {};
-            vm.model.isUserAuthKeysEmpty = true;
-            $state.go('signIn');
-        }
-
-        function checkUserAuthData() {
-            return $resource('http://dev-api.mobile.design/api/users').get().$promise.then(
-                response => {},
-                response => {
-                    /*  
+    function checkUserAuthData() {
+      return $resource('http://dev-api.mobile.design/api/users')
+        .get()
+        .$promise.then(
+          response => {},
+          response => {
+            /*  
                     -- Another method instead of mentioned below ('ms'-parameter in $timeout can be adjustable with toast showing time and less than one).
                     -- There is no need the onHidden event in app.run(), but it needs to add 'tapToDismiss: true' instead.
                     --------------------------------------------------------------------------------------------------------------------------------------
@@ -100,33 +113,35 @@
                             $state.go('signIn');
                         }, 6000).then(() => $q.reject());
                      */
-                    console.log(response);
-                    toastr.error('For authorized users only', 'Warning');
-                    let defer = $q.defer();
+            console.log(response);
+            toastr.error('For authorized users only', 'Warning');
+            let defer = $q.defer();
 
-                    $rootScope.$on('authToastrClosed', () => {
-                        $timeout(function() { $state.go('signIn') }, 500);
-                        defer.reject();
-                    });
+            $rootScope.$on('authToastrClosed', () => {
+              $timeout(function() {
+                $state.go('signIn');
+              }, 500);
+              defer.reject();
+            });
 
-                    return defer.promise;
-                }
-            )
-        }
-
-        function setUserAuthData(param) {
-            let headers = param.headers;
-            vm.model.user.accesstoken = headers.accesstoken;
-            vm.model.user.client = headers.client;
-            vm.model.user.expiry = headers.expiry;
-            vm.model.user.uid = headers.uid;
-            vm.model.user.tokentype = headers.tokentype;
-            vm.model.isUserAuthKeysEmpty = false;
-        }
-
-        function redirectAfterAuth(param) {
-            setUserAuthData(param);
-            $state.go('dashboard');
-        }
+            return defer.promise;
+          }
+        );
     }
+
+    function setUserAuthData(param) {
+      let headers = param.headers;
+      vm.model.user.accesstoken = headers.accesstoken;
+      vm.model.user.client = headers.client;
+      vm.model.user.expiry = headers.expiry;
+      vm.model.user.uid = headers.uid;
+      vm.model.user.tokentype = headers.tokentype;
+      vm.model.isUserAuthKeysEmpty = false;
+    }
+
+    function redirectAfterAuth(param) {
+      setUserAuthData(param);
+      $state.go('dashboard');
+    }
+  }
 })();
